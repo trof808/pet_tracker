2917eda4a9e3198e38022a5cde9173ce463abdf0
Tim <tim@MacBook-Pro-2.local>
2025-04-24 01:33:36 +0300

    updated the structure

--- src/features/MonthDays/CalendarDateSliderFeature.tsx
+++ src/features/MonthDays/CalendarDateSliderFeature.tsx
@@ -4,27 +4,6 @@ import { useSelectCurrentDay } from './hooks/useSelectCurrentDay';
 
 // переписать на date-fns
 export const CalendarDateSlider = (): JSX.Element => {
-  // const currentDate = useSelector(({ filters }: { filters: { date: FiltersDate }}) => filters.date);
-  // const dispatch = useDispatch();
-
-  // const [yearStr, monthStr, dayStr] = currentDate.split('-');
-  // const selectedDay = parseInt(dayStr);
-
-  // const dates = eachDayOfInterval({
-  //   start: startOfMonth(new Date(currentDate)),
-  //   end: endOfMonth(new Date(currentDate)),
-  // });
-  // const formattedDates = dates.map((date) => ({
-  //   day: parseInt(format(date, 'd')),
-  //   weekday: format(date, 'EEEEEE', { locale: ru }),
-  // }));
-
-  // const handleDateClick = (day: number) => {
-  //   const newDayStr = day.toString().padStart(2, '0');
-  //   const newDate = `${yearStr}-${monthStr}-${newDayStr}`;
-  //   dispatch(setDate(newDate));
-  // };
-
   const { formattedDates, selectedDay, handleDateClick } = useSelectCurrentDay();
 
   return (
