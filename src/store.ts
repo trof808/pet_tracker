77380ae6b78390aa9df6a6cbc1a782109b948a0d
Tim <tim@MacBook-Pro-2.local>
2025-05-02 23:17:42 +0300

    updated project structure

--- src/store.ts
+++ src/store.ts
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
-import userInfoReducer from '../entities/auth/services/model/userInfoSlice';
-import filtersReducer from '../entities/calendar/ui/model/filtersSlice';
+import userInfoReducer from './entities/auth/services/model/userInfoSlice';
+import filtersReducer from './entities/calendar/ui/model/filtersSlice';
 
 
 export const store = configureStore({
